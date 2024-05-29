750bc9e6156ccadad6a8f21ba7145948658d283f
suryasdas <srdas73@gmail.com>
2024-05-29 00:10:49 -0400

    added branding view

--- ShopGPT/Views/BrandingView.swift
+++ ShopGPT/Views/BrandingView.swift
@@ -2,16 +2,28 @@
 //  BrandingView.swift
 //  ShopGPT
 //
-//  Created by Surya Das on 5/29/24.
+//  Created by Surya Das on 5/28/24.
 //
 
 import SwiftUI
 
 struct BrandingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+
+            Text("ShopGPT")
+                .font(.system(size: 45, weight: .bold, design: .rounded)) // Specified size, bold weight, rounded design
+                .foregroundColor(.black)
+                .padding(.top, 20) // Adds padding at the top of the text
+            // Pushes the text to the top
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top) // Ensures VStack fills the parent and aligns text to the top
+        
+
+        
     }
 }
+
 
 #Preview {
     BrandingView()
